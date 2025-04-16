342a721df0e28ce45c9cfc7ae859fd81094d944f
Uvazzz <uvsekhon.official@gmail.com>
2025-04-17 01:08:52 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import os
 from port_scanner import run_port_scanner
+from pass_r import check_password_strength
 
 def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -9,19 +10,34 @@ def clear():
 def main():
     while True:
         clear()
-        print("┌─[ Vulnerability Detector ]")
+
+        print("┌─[ Toolkit for Engineers ]")
         print("│  1. Port Scanner")
+        print("│  2. Check Password Strength")
         print("│  0. Exit")
         print("└─ Choose an option:")
         
         choice = input("shell_$~ ").strip()
 
         if choice == '1':
+
             run_port_scanner()
+
+        elif choice == '2':
+
+            password = input("Enter password to check: ")
+
+            check_password_strength(password)
+
+            input("\nPress Enter to Continue...")
+
         elif choice == '0':
-            print("\n[+] Exiting... Stay safe, hacker 😎")
+
+            print("\n[+] Exiting... Stay safe, bruv 😎")
             break
+
         else:
+            
             print("\n[!] Invalid option.")
             input("Press Enter to continue...")
 
